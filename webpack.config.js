370b95e98e1de0d4a0e0cc4743a7740f9d15fe88
Wesley Cabus <wesley@gotsharp.be>
2016-10-06 20:50:58 +0200

    Fixed images

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,9 @@ module.exports = {
       { test: /\.less$/, loader: 'style!css!less' },
       { test: /\.css$/, loader: 'style!css' },
       { test: /\.html$/, loader: 'html-loader' },
-      { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?limit=10000&minetype=application/font-woff" },
+      { test: /\.(png|jpg)$/, loader: 'file-loader' },
+      { test: /\.ico$/, loader: 'file-loader' },
+      { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?limit=10000&mimetype=application/font-woff" },
       { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader" }
     ]
   }
