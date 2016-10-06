8682ab1ba506461ffb32c95d94af0677a4128ffb
Wesley Cabus <Wesley.Cabus@realdolmen.com>
2016-10-06 14:26:13 +0200

    We haz chips

--- src/app/root/home/home.component.ts
+++ src/app/root/home/home.component.ts
@@ -1,15 +1,42 @@
 import { AppController} from '../root.component';
 
+interface IChip {
+  value: number,
+  color: string
+}
+
 export class HomeController {
   parent: AppController;
+  chips: IChip[];
+  smallBlind: number;
+  bigBlind: number;
 
   constructor() {
+    this.chips = [{
+      value: 25,
+      color: 'white'
+    }, {
+      value: 100,
+      color: 'blue'
+    }, {
+      value: 500,
+      color: 'green'
+    }, {
+      value: 1000,
+      color: 'red'
+    }, {
+      value: 5000,
+      color: 'black'
+    }];
+
+    this.smallBlind = 25;
+    this.bigBlind = 50;
   }
 }
 
 export default {
   controller: HomeController,
-  template: `jos`,
+  template: <string> require('./home.html'),
   require: {
     parent: '^appRoot'
   }
